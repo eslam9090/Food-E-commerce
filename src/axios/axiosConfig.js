93dec0b56b4984afe4482233abe9f46ac6f28e53
eslam9090 <eslammyaakoup7090@gmail.com>
2025-05-20 10:18:49 +0300

    up13

--- src/axios/axiosConfig.js
+++ src/axios/axiosConfig.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// eslint-disable-next-line no-undef
-const BASE_URL = process.env.REACT_APP_API_URL;
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 const axiosConfig = axios.create({
   baseURL: BASE_URL,
